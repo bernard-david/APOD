00d0927e08ac272808dcc7885cd00e239c947d53
bernard-david <dpbernard18@gmail.com>
2021-09-24 19:13:35 -0400

    added apikey and test request

--- client/src/components/NasaPhoto.jsx
+++ client/src/components/NasaPhoto.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const apiKey = process.env.REACT_APP_NASA_KEY;
 
 const NasaPhoto = props => {
+
+    const [nasaData, setNasaData] = useState(null);
+
+    useEffect(() => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }, []);
+
     return (
         <div>
-
+            <p>Nasa data page</p>
         </div>
     )
 }
